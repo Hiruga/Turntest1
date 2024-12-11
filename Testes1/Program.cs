fe93c6f72f412a310b07842aa666da0c4e364b0f
Hiruga <hirugahasagui@gmail.com>
2024-12-11 18:42:13 -0300

    testando while pra substituir goto

--- Testes1/Program.cs
+++ Testes1/Program.cs
@@ -24,147 +24,148 @@ namespace Testes1
             int eHP, pHP, eDMG, pDMG;
             bool pBlock = false;
             bool eBlock = false;
+            bool combat = true;
             eHP = int.Parse("20");
             pHP = int.Parse("20");
             eDMG = int.Parse("2");
             pDMG = int.Parse("3");
             int turn = rng.Next(2); // Só pode ser 0 e 1
-
-        iCombate:
-            Console.Clear();
-            char pAction;
-            int eAction = rng.Next(3); // Pode ser 0, 1 e 2
-
-            if (eHP <= 0 || pHP <= 0)
+            while (combat == true)
             {
-                string mensagem;
+                Console.Clear();
+                char pAction;
+                int eAction = rng.Next(3); // Pode ser 0, 1 e 2
 
-                Console.WriteLine("Combate encerrado.");
-                mensagem =
-                eHP <= 0 ? "Você venceu!" : "Você perdeu...";
-                Console.WriteLine(mensagem);
-            }
-            else
-            {
-                if (turn == 0)
+                if (eHP <= 0 || pHP <= 0)
                 {
-                    eBlock = false;
-                    Console.WriteLine("Inimigo teste");
-                    Console.WriteLine("{0} HP", eHP);
-                    Console.WriteLine("{0} Dano", eDMG);
-                    Console.WriteLine("");
+                    string mensagem;
 
-                    switch (eAction)
-                    {
-                        case 0:
-                            Console.WriteLine("Inimigo teste o atacou!");
-                            if (pBlock == true)
-                            {
-                                Console.WriteLine("Você consegue se defender com sucesso!");
-                                pHP = pHP - (eDMG - 2);
-                                Console.WriteLine("Você sofreu {0} de dano!", eDMG - 2);
-                                pBlock = false;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Você recebeu {0} de dano!", eDMG);
-                                pHP = pHP - eDMG;
-                            }
-                            break;
-
-                        case 1:
-                            if (eHP >= 20)
-                            {
-                                Console.WriteLine("Seu inimigo tentou se curar, mas já estava de vida cheia.");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Seu inimigo se curou!");
-                                eHP++;
-                            }
-                            break;
-
-                        case 2:
-                            Console.WriteLine("Seu inimigo levanta sua guarda!");
-                            eBlock = true;
-                            break;
-                    }
-                    Console.ReadKey();
-                    turn = 1;
-                    goto iCombate;
+                    Console.WriteLine("Combate encerrado.");
+                    mensagem =
+                    eHP <= 0 ? "Você venceu!" : "Você perdeu...";
+                    Console.WriteLine(mensagem);
+                    combat = false;
                 }
                 else
                 {
-                    pBlock = false;
-                    Console.WriteLine("Player beta");
-                    Console.WriteLine("{0} HP", pHP);
-                    Console.WriteLine("{0} Dano", pDMG);
-                    Console.WriteLine("Atacar (A)");
-                    Console.WriteLine("Curar (S)");
-                    Console.WriteLine("Defender (D)");
-
-                    pAction = char.Parse(Console.ReadLine());
-
-                    switch (pAction)
+                    if (turn == 0)
                     {
-                        case 'A':
-                        case 'a':
-                            if (eBlock == true)
-                            {
-                                Console.WriteLine("Seu inimigo se defendeu!");
-                                eHP = eHP - (pDMG - 2);
-                                eBlock = false;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Seu ataque causou {0} de dano!", pDMG);
-                                eHP = eHP - pDMG;
-                            }
-                            break;
+                        eBlock = false;
+                        Console.WriteLine("Inimigo teste");
+                        Console.WriteLine("{0} HP", eHP);
+                        Console.WriteLine("{0} Dano", eDMG);
+                        Console.WriteLine("");
 
-                        case 'S':
-                        case 's':
-                            if (pHP >= 20)
-                            {
-                                Console.WriteLine("Vida já está cheia!");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Curou 1 de vida!");
-                                pHP++;
-                            }
-                            break;
+                        switch (eAction)
+                        {
+                            case 0:
+                                Console.WriteLine("Inimigo teste o atacou!");
+                                if (pBlock == true)
+                                {
+                                    Console.WriteLine("Você consegue se defender com sucesso!");
+                                    pHP = pHP - (eDMG - 2);
+                                    Console.WriteLine("Você sofreu {0} de dano!", eDMG - 2);
+                                    pBlock = false;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Você recebeu {0} de dano!", eDMG);
+                                    pHP = pHP - eDMG;
+                                }
+                                break;
 
-                        case 'D':
-                        case 'd':
-                            Console.WriteLine("Você levanta sua guarda!");
-                            pBlock = true;
-                            break;
+                            case 1:
+                                if (eHP >= 20)
+                                {
+                                    Console.WriteLine("Seu inimigo tentou se curar, mas já estava de vida cheia.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Seu inimigo se curou!");
+                                    eHP++;
+                                }
+                                break;
 
-                        default:
-                            Console.WriteLine("Ação inválida");
-                            break;
+                            case 2:
+                                Console.WriteLine("Seu inimigo levanta sua guarda!");
+                                eBlock = true;
+                                break;
+                        }
+                        Console.ReadKey();
+                        turn = 1;
                     }
-                    Console.ReadKey();
-                    turn = 0;
-                    goto iCombate;
-                }
+                    else
+                    {
+                        pBlock = false;
+                        Console.WriteLine("Player beta");
+                        Console.WriteLine("{0} HP", pHP);
+                        Console.WriteLine("{0} Dano", pDMG);
+                        Console.WriteLine("Atacar (A)");
+                        Console.WriteLine("Curar (S)");
+                        Console.WriteLine("Defender (D)");
 
-            }
-            Console.WriteLine("Caso deseja encerrar o programa pressione qualquer tecla.");
-            Console.WriteLine("Pressione R para recomeçar o combate.");
-            escolha = char.Parse(Console.ReadLine());
-            if (escolha == 'r' || escolha == 'R')
-            {
-                goto inicio;
-            }
-            else
-            {
-                Console.WriteLine("Programa encerrado.");
-                Console.WriteLine("Pressione qualquer tecla para fechar.");
-                Console.WriteLine("");
-                Console.ReadKey();
-                Environment.Exit(0);
+                        pAction = char.Parse(Console.ReadLine());
+
+                        switch (pAction)
+                        {
+                            case 'A':
+                            case 'a':
+                                if (eBlock == true)
+                                {
+                                    Console.WriteLine("Seu inimigo se defendeu!");
+                                    eHP = eHP - (pDMG - 2);
+                                    eBlock = false;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Seu ataque causou {0} de dano!", pDMG);
+                                    eHP = eHP - pDMG;
+                                }
+                                break;
+
+                            case 'S':
+                            case 's':
+                                if (pHP >= 20)
+                                {
+                                    Console.WriteLine("Vida já está cheia!");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Curou 1 de vida!");
+                                    pHP++;
+                                }
+                                break;
+
+                            case 'D':
+                            case 'd':
+                                Console.WriteLine("Você levanta sua guarda!");
+                                pBlock = true;
+                                break;
+
+                            default:
+                                Console.WriteLine("Ação inválida");
+                                break;
+                        }
+                        Console.ReadKey();
+                        turn = 0;
+                    }
+
+                }
+                }
+                Console.WriteLine("Caso deseja encerrar o programa pressione qualquer tecla.");
+                Console.WriteLine("Pressione R para recomeçar o combate.");
+                escolha = char.Parse(Console.ReadLine());
+                if (escolha == 'r' || escolha == 'R')
+                {
+                    goto inicio;
+                }
+                else
+                {
+                    Console.WriteLine("Programa encerrado.");
+                    Console.WriteLine("Pressione qualquer tecla para fechar.");
+                    Console.WriteLine("");
+                    Console.ReadKey();
+                    Environment.Exit(0);
             }
         }
     }
